c457f7dd08f4f48e3cbc1a44b1cdf044afb44170
lokth <thapa.lok19@gmail.com>
2017-12-13 11:49:18 -0500

    first commit

---
+++ src/main/java/com/thisistime/springamqp/SpringAMQPRabbitAsyncListener.java
@@ -0,0 +1,14 @@
+package com.thisistime.springamqp;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+public class SpringAMQPRabbitAsyncListener implements MessageListener{
+
+	@Override
+	public void onMessage(Message msg) {
+		System.out.println("Listener received message = "+ new String(msg.getBody()));
+		
+	}
+
+}
